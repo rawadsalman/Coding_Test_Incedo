efd574f88bbda6b4d23d7bb402fca15ce7b8010a
Rawad Salman <rwadsalman81@gmail.com>
2022-05-31 13:34:01 +0200

    changing how to get random artist names in getRandomArtistNames function

--- index.mjs
+++ index.mjs
@@ -4,65 +4,49 @@ import 'dotenv/config';
 import fetch from 'node-fetch';
 
 const api_key=process.env.API_KEY;
-const artist = 'cher';
 
+var artistName = 'cher';
 var body = '';
 var randomArtist=[];
+var rand =0;
 
-function JSONtoCSV(jsonArray){
+const JSONtoCSV =(jsonArray) =>{
     body += "name"  +  ', ' + "mbid"+  ', ' + "url"+  ', ' +"image_small"+  ', ' +"image"+'\r\n';
     for(var i=0; i<jsonArray.length; i++){
         var item = jsonArray[i];
-        console.log(item);
-        body += item.name  + ', ' +item.mbid +  ', ' + item.url+  ', ' + item.image[0].size+  ', ' + item.image[0]['#text']+ '\r\n';    
-    
+        body += item.name  + ', ' +item.mbid +  ', ' + item.url+  ', ' + item.image[0].size+  ', ' + item.image[0]['#text']+ '\r\n';
     }
-    return body;
+       fs.writeFileSync("result.csv", body);
 }
-const getRandomArtist = (length = 2) => {
 
-    let chars = 'abcdefghijklmnopqrstuvwxyz';
-    let str = '';
-    for (let i = 0; i < length; i++) {
-        str += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
+const  between =(min, max) => {
+  return Math.floor(
+    Math.random() * (max - min + 1) + min
+  )
+}
+const getRandomArtistNames = () => {
+  console.log("getting random Artist Names");
+  fetch("http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher"+"&api_key="+api_key+"&format=json")
+   .then((res) => res.json())
+   .then((json) => {
+       while (randomArtist.length < 5){
+           rand = between (0, json.results.artistmatches.artist.length);
+           randomArtist.push(json.results.artistmatches.artist[rand].name);
+       }
+    fs.writeFileSync("result.csv",randomArtist.toString());
+  })
+}
 
-    return str;
+const main = () => {
 
-};
-
-function getRandomArtistNames(){
-const randomArtist =getRandomArtist (2)
-fetch("http://ws.audioscrobbler.com/2.0/?method=artist.search&artist="+randomArtist+"&api_key="+api_key+"&format=json")
-
-.then((res) => res.json())
-
-.then((json) =>{
-    console.log(json.results.artistmatches.artist.length);
-    if (json.results.artistmatches.artist.length > 0){
-     if(randomArtist.length < 3)
-     randomartist.push(json.results.artistmatches.artist.name)
+   fetch("http://ws.audioscrobbler.com/2.0/?method=artist.search&artist="+artistName+"&api_key="+api_key+"&format=json")
+   .then((res) => res.json())
+   .then((json) => {
+     if( json.results.artistmatches.artist.length > 0 )
+        JSONtoCSV(json.results.artistmatches.artist);
      else
-        return randomartist;
-     }
-     else
-     getRandomArtistNames();
+        getRandomArtistNames();
  })
 }
-
-fetch("http://ws.audioscrobbler.com/2.0/?method=artist.search&artist="+artist+"&api_key="+api_key+"&format=json")
-
-.then((res) => res.json())
-.then((json) =>{
-    console.log("json length ..."+json.results.artistmatches.artist.length);
-   if( json.results.artistmatches.artist.length > 0 ){
-      body= JSONtoCSV(json.results.artistmatches.artist);
-      fs.writeFileSync("result.csv", body);
-   }
-   else{
-      console.log("getting random Artist Names");
-      getRandomArtistNames();
-   }
-})
-.catch(error => console.log('error:', error));
+ main();
 
